c0951e8dbf598b0fc87212ebdc60190972cf5fb6
SamarMedhat7 <samarmedhat573@gmail.com>
2024-05-27 00:04:36 +0300

    refactor: customize url validation

--- check/models.py
+++ check/models.py
@@ -2,6 +2,4 @@ from django.db import models
 
 # Create your models here.
 
-class URL(models.Model):
-    url = models.URLField()
     
--- check/serializers.py
+++ check/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from .models import URL
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 
-class URLSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = URL
-        fields = ('url',)
+
+class URLSerializer(serializers.Serializer):
+    url = serializers.CharField()
+
+    def validate_url(self, value):
+        # Validate URL
+        url_validator = URLValidator()
+        try:
+            url_validator(value)
+        except ValidationError:
+            raise serializers.ValidationError("Invalid URL")
+        
+        return value
