72f0b46730be51589d1e09697ad675d8f3dc2c41
SamarMedhat7 <samarmedhat573@gmail.com>
2024-05-27 02:40:44 +0300

    refactor: extract domain function

--- check/utils.py
+++ check/utils.py
@@ -4,8 +4,13 @@ from urllib.parse import urlparse
 
 def extract_domain(url):
     parsed_url = urlparse(url)
-    domain = parsed_url.netloc
-    return domain
+    if parsed_url.netloc:
+        domain = parsed_url.netloc.lower()
+        if domain.startswith("www."):
+            domain = domain[4:]  # Remove "www." prefix
+        return domain
+    else:
+        return None
     
         
 
