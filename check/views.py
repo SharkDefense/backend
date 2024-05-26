a35ce006903c7d63bb8039809d21843d91125602
SamarMedhat7 <samarmedhat573@gmail.com>
2024-05-26 23:38:56 +0300

    style

--- check/views.py
+++ check/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import URLSerializer
 from .machine_model.ml import predict
-from rest_framework.permissions import IsAuthenticated,AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .utils import extract_domain
